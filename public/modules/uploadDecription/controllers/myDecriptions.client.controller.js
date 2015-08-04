b319ac52246987424b9ad7bab6a0f077e9a51fa0
Eliav Maman <eliav@myinpact.com>
2015-08-04 19:58:47 +0300

    asd

--- public/modules/uploadDecription/controllers/myDecriptions.client.controller.js
+++ public/modules/uploadDecription/controllers/myDecriptions.client.controller.js
@@ -1,23 +1,32 @@
 'use strict';
 
 angular.module('uploadDecription').controller('MyDecriptionController',
-          ['$scope', '$http', '$location', 'Authentication', '$timeout',
-  function ($scope, $http, $location, Authentication, $timeout) {
-    $scope.authentication = Authentication;
+  ['$scope', '$http', '$location', 'Authentication', '$timeout',
+    function ($scope, $http, $location, Authentication, $timeout) {
+      $scope.authentication = Authentication;
 
-    // If user is signed in then redirect back home
-    //   if ($scope.authentication.user) $location.path('/');
+      // If user is signed in then redirect back home
+      //   if ($scope.authentication.user) $location.path('/');
 
-    $scope.init = function () {
-      $scope.getUserDecription();
-    };
-    $scope.getUserDecription = function () {
-      $http.get('/decriptions', {params: {email: $scope.authentication.user.email}}).success(function (response) {
+      $scope.init = function () {
+        $scope.getUserDecription();
+      };
+      $scope.getUserDecription = function () {
+        $http.get('/decriptions', {params: {email: $scope.authentication.user.email}}).success(function (response) {
 
-        $scope.decriptions = response;
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
-  }
-]);
+
+          var decriptionList = response;
+          for (var i = 0; i < decriptionList.length; i++) {
+            var d = new Date(decriptionList[i].created);
+            var formatedDate = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear();
+            decriptionList[i].created = formatedDate;
+          }
+          $scope.decriptions = decriptionList;
+
+
+        }).error(function (response) {
+          $scope.error = response.message;
+        });
+      };
+    }
+  ]);
